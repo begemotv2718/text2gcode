2ac37d9d168adbbd409a4176f79ea58238cbc75d
begemotv2718 <begemotv2718@gmail.com>
2015-07-28 01:33:41 +0300

    Some example

--- text2gcode.py
+++ text2gcode.py
@@ -1,32 +1,40 @@
+# -*- coding: utf-8 -*-
 import cairo
 
-def text_extent(font, font_size, text, *args, **kwargs):
-    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 0, 0)
-    ctx = cairo.Context(surface)
-    ctx.select_font_face(font, *args, **kwargs)
-    ctx.set_font_size(font_size)
-    return ctx.text_extents(text)
 
-text='Example'
+text=u'Хуй войне'
 font="Sans"
 font_size=55.0
 font_args=[cairo.FONT_SLANT_NORMAL]
-#(x_bearing, y_bearing, text_width, text_height,
-# x_advance, y_advance) = text_extent(font, font_size, text, *font_args)
-text_width,text_height=1000,1000
-surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, int(text_width), int(text_height))
+
+surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 1, 1)
 ctx = cairo.Context(surface)
 ctx.select_font_face(font, *font_args)
 ctx.set_font_size(font_size)
-#ctx.move_to(-x_bearing, -y_bearing)
+x_bearing,y_bearing,text_width,text_height,x_advance,y_advance=ctx.text_extents(text)
+ctx.move_to(-x_bearing, -y_bearing)
 ctx.text_path(text)
-pts=ctx.copy_path_flat()
+#pts=ctx.copy_path_flat()
+pts=ctx.copy_path()
+cur_segment_start=(0.0,0.0)
+g_code=[]
 for p in pts:
-  print p
-#ctx.set_source_rgb(0.47, 0.47, 0.47)
-#ctx.fill_preserve()
-ctx.set_source_rgb(1, 0, 0)
-ctx.set_line_width(1.5)
-ctx.stroke()
+  if p[0] == 0:
+    g_code.append('G1 Z3.0')
+    g_code.append('G0 X%2.5f Y%2.5f' % p[1])
+    g_code.append('G1 F100.0 Z-2.0')
+    cur_segment_start=p[1]
+  elif(p[0] == 1):
+    g_code.append('G1 X%2.5f Y%2.5f' % p[1])
+  elif(p[0] == 2):
+    start=(p[1][0],p[1][1])
+    middle=(p[1][2],p[1][3])
+    end=(p[1][4],p[1][5])
+    g_code.append('G2 X%2.5f Y%2.5f I%2.5f J%2.5f' % (start[0],start[1],0.0,0.0))
+  elif(p[0] == 3):
+    g_code.append('G1 X%2.5f Y%2.5f' % cur_segment_start)
 
-surface.write_to_png("/tmp/out.png")
+
+
+for l in g_code:
+  print l
