a10cd4a5db6cb5dc2646b85ae23698fe6536fc47
begemotv2718 <begemotv2718@gmail.com>
2015-07-29 21:43:00 +0300

    Mirror the picture

--- text2gcode.py
+++ text2gcode.py
@@ -2,7 +2,7 @@
 import cairo
 
 
-text=u'Хуй войне'
+text=u'HELLO HS'
 font="Sans"
 font_size=20.0
 font_args=[cairo.FONT_SLANT_NORMAL]
@@ -22,12 +22,12 @@ g_code=['G21 G90 G64 G40','G0 Z3.0','M3 S10000']
 for p in pts:
   if p[0] == 0:
     g_code.append('G0 Z3.0')
-    g_code.append('G0 X%2.5f Y%2.5f' % p[1])
+    g_code.append('G0 X%2.5f Y%2.5f' % (p[1][0],text_height-p[1][1]))
     g_code.append('G1 F100.0 Z-2.0')
-    cur_segment_start=p[1]
+    cur_segment_start=(p[1][0],text_height-p[1][1])
     feed="F400.0"
   elif(p[0] == 1):
-    g_code.append('G1 %s X%2.5f Y%2.5f' % (feed,p[1][0],p[1][1]))
+    g_code.append('G1 %s X%2.5f Y%2.5f' % (feed,p[1][0],text_height-p[1][1]))
     feed=""
   elif(p[0] == 2):
     start=(p[1][0],p[1][1])
